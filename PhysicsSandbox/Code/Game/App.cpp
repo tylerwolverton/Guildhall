43f66590ab27c621058b4dd119c284e0ed46b3da
Tyler Wolverton <tylerwolvert@gmail.com>
2020-01-28 16:37:07 -0600

    Created project for MP2 physics sandbox

---
+++ PhysicsSandbox/Code/Game/App.cpp
@@ -0,0 +1,169 @@
+#include "App.hpp"
+#include "Engine/Core/EngineCommon.hpp"
+#include "Engine/Core/Rgba8.hpp"
+#include "Engine/Core/Vertex_PCU.hpp"
+#include "Engine/Core/Time.hpp"
+#include "Engine/Core/EventSystem.hpp"
+#include "Engine/Core/DevConsole.hpp"
+#include "Engine/Math/RandomNumberGenerator.hpp"
+#include "Engine/Math/MathUtils.hpp"
+#include "Engine/Renderer/RenderContext.hpp"
+#include "Engine/Renderer/Camera.hpp"
+#include "Engine/Input/InputSystem.hpp"
+#include "Engine/Audio/AudioSystem.hpp"
+#include "Game/GameCommon.hpp"
+#include "Game/Game.hpp"
+
+
+//-----------------------------------------------------------------------------------------------
+App* g_app = nullptr;				    // Created and owned by Main_Windows.cpp
+InputSystem* g_inputSystem = nullptr;	// Created and owned by the App
+DevConsole* g_devConsole = nullptr;		// Created and owned by the App
+RenderContext* g_renderer = nullptr;	// Created and owned by the App
+Game* g_game = nullptr;					// Created and owned by the App
+BitmapFont* g_testFont = nullptr;
+
+
+//-----------------------------------------------------------------------------------------------
+App::App()
+{
+	
+}
+
+
+//-----------------------------------------------------------------------------------------------
+App::~App()
+{
+	
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::Startup()
+{
+	g_eventSystem = new EventSystem();
+	g_devConsole = new DevConsole();
+	g_inputSystem = new InputSystem();
+	g_renderer = new RenderContext();
+	g_game = new Game();
+	
+	g_eventSystem->Startup();
+	g_devConsole->Startup();
+	g_inputSystem->Startup();
+	g_renderer->Startup();
+	g_game->Startup();
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::Shutdown()
+{
+	g_game->Shutdown();
+	g_renderer->Shutdown();
+	g_inputSystem->Shutdown();
+	g_devConsole->Shutdown();
+	g_eventSystem->Shutdown();
+
+	delete g_game;
+	g_game = nullptr;
+
+	delete g_renderer;
+	g_renderer = nullptr;
+	
+	delete g_inputSystem;
+	g_inputSystem = nullptr;
+
+	delete g_devConsole;
+	g_devConsole = nullptr;
+
+	delete g_eventSystem;
+	g_eventSystem = nullptr;
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::RunFrame()
+{
+	static double timeLastFrameStarted = GetCurrentTimeSeconds(); // Runs once only!	
+	double timeThisFrameStarted = GetCurrentTimeSeconds();
+	double deltaSeconds = timeThisFrameStarted - timeLastFrameStarted;
+	timeLastFrameStarted = timeThisFrameStarted;
+
+	BeginFrame();											// for all engine systems (NOT the game)
+	Update( ClampMinMax( (float)deltaSeconds, 0.f, 0.1f) ); // for the game only
+	Render();												// for the game only
+	EndFrame();												// for all engine systems (NOT the game)
+}
+
+
+//-----------------------------------------------------------------------------------------------
+bool App::HandleQuitRequested()
+{
+	m_isQuitting = true;
+
+	return 0;
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::RestartGame()
+{
+	g_game->Shutdown();
+	delete g_game;
+
+	g_game = new Game();
+	g_game->Startup();
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::BeginFrame()
+{
+	g_eventSystem->BeginFrame();
+	g_devConsole->BeginFrame();
+	g_inputSystem->BeginFrame();
+	g_renderer->BeginFrame();
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::Update( float deltaSeconds )
+{
+	UpdateFromKeyboard( deltaSeconds );
+
+	g_game->Update( deltaSeconds );
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::UpdateFromKeyboard( float deltaSeconds )
+{
+	UNUSED( deltaSeconds );
+	
+	if ( g_inputSystem->WasKeyJustPressed( KEY_ESC ) )
+	{
+		HandleQuitRequested();
+	}
+
+	if ( g_inputSystem->WasKeyJustPressed( KEY_F8 ) )
+	{
+		g_app->RestartGame();
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::Render() const
+{
+	g_game->Render();
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void App::EndFrame()
+{
+	g_renderer->EndFrame();
+	g_inputSystem->EndFrame();
+	g_devConsole->EndFrame();
+	g_eventSystem->EndFrame();
+}
---
+++ PhysicsSandbox/Code/Game/Game.hpp
@@ -0,0 +1,105 @@
+#pragma once
+#include "Engine/Math/Vec2.hpp"
+#include "Engine/Math/AABB2.hpp"
+#include "Engine/Math/OBB2.hpp"
+#include "Engine/Math/Capsule2.hpp"
+#include "Engine/Math/Polygon2.hpp"
+#include "Engine/Core/EngineCommon.hpp"
+#include "Engine/Core/Rgba8.hpp"
+
+
+//-----------------------------------------------------------------------------------------------
+class Entity;
+struct Rgba8;
+class RandomNumberGenerator;
+class Camera;
+
+
+//-----------------------------------------------------------------------------------------------
+enum MouseState
+{
+	MOUSE_STATE_POINT,
+	MOUSE_STATE_OBB2,
+
+	MOUSE_STATE_NUM_STATES
+};
+
+
+//-----------------------------------------------------------------------------------------------
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	void Startup();
+	void Update( float deltaSeconds );
+	void Render() const;
+	void DebugRender() const;
+	void Shutdown();
+
+	void RestartGame();
+	
+public:
+	RandomNumberGenerator* m_rng = nullptr;
+
+private:
+	void RandomizeShapes();
+	void RandomizeLine();
+	void RandomizeDisc();
+	void RandomizeAABB2();
+	void RandomizeOBB2();
+	void RandomizeCapsule2();
+	void RandomizePolygon2();
+
+	void RenderMouseShape() const;
+	void RenderShapes() const;
+	void RenderLine() const;
+	void RenderDisc() const;
+	void RenderAABB2() const;
+	void RenderOBB2() const;
+	void RenderCapsule2() const;
+	void RenderPolygon2() const;
+
+	void RenderNearestPoints() const;
+
+	void UpdateFromKeyboard( float deltaSeconds );
+	void UpdateCameras( float deltaSeconds );
+	void UpdateMousePosition();
+	void UpdateNearestPoints();
+	void UpdateShapeColors();
+
+private:
+	bool		m_isDebugRendering = false;
+
+	Camera*		m_worldCamera = nullptr;
+	Camera*		m_uiCamera = nullptr;
+
+	MouseState  m_mouseState = MOUSE_STATE_POINT;
+	Vec2		m_mouseWorldPosition = Vec2::ZERO;
+	OBB2		m_mouseOBB2;
+
+	Vec2		m_lineSegmentStart = Vec2::ZERO;
+	Vec2		m_lineSegmentVector = Vec2::ZERO;
+
+	Vec2		m_discCenter = Vec2::ZERO;
+	float		m_discRadius = 0.f;
+
+	AABB2		m_aabb2 = AABB2::ONE_BY_ONE;
+	OBB2		m_obb2;
+	Capsule2	m_capsule2;
+	Polygon2	m_polygon2;
+
+	Vec2		m_nearestPointOnLine = Vec2::ZERO;
+	Vec2		m_nearestPointOnDisc = Vec2::ZERO;
+	Vec2		m_nearestPointOnAABB2 = Vec2::ZERO;
+	Vec2		m_nearestPointOnOBB2 = Vec2::ZERO;
+	Vec2		m_nearestPointOnCapsule2 = Vec2::ZERO;
+
+	Rgba8		m_lineColor = Rgba8::CYAN;
+	Rgba8		m_discColor = Rgba8::GREEN;
+	Rgba8		m_AABB2Color = Rgba8::MAGENTA;
+	Rgba8		m_OBB2Color = Rgba8::ORANGE;
+	Rgba8		m_capsule2Color = Rgba8::RED;
+	Rgba8		m_polygon2Color = Rgba8::PURPLE;
+};
