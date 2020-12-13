1c8bf6c93ef90aaf2eb8c2a6f411f97d5ecd14e3
Tyler Wolverton <tylerwolvert@gmail.com>
2020-12-13 15:54:58 -0600

    PlaqueOps: Error on nonexistent script

--- DecayJam/Code/Game/EntityDefinition.cpp
+++ DecayJam/Code/Game/EntityDefinition.cpp
@@ -137,6 +137,11 @@ EntityDefinition::EntityDefinition( const XmlElement& entityDefElem, SpriteSheet
 		if ( !m_zephyrScriptName.empty() )
 		{
 			m_zephyrScriptDef = ZephyrScriptDefinition::GetZephyrScriptDefinitionByName( m_zephyrScriptName );
+			if ( m_zephyrScriptDef == nullptr )
+			{
+				g_devConsole->PrintError( Stringf( "EntityTypes.xml '%s': Script '%s' does not exist", m_type.c_str(), m_zephyrScriptName.c_str() ) );
+				return;
+			}
 
 			// Parse initial values
 			const XmlElement* scriptVarInitElem = scriptElem->FirstChildElement( "ScriptVarInit" );
